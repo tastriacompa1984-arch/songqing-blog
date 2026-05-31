d4699278e11b8551e55b85289498799b48aa2f04
tastriacompa1984-arch <tastriacompa1984-arch@users.noreply.github.com>
2026-05-31 13:53:14 +0000

    deploy: 88d4c90869630f77be0bc6a8fb00b25c31c2da9b

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/undefined.html","posts/undefined.html","posts/undefined.html","posts/undefined.html"];function toRandomPost(){
+var posts=["posts/undefined.html","posts/undefined.html","posts/undefined.html","posts/undefined.html","posts/undefined.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
