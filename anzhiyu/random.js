db9b40c707eb7448dd2f75125c2aaddbcad79e29
tastriacompa1984-arch <tastriacompa1984-arch@users.noreply.github.com>
2026-06-04 14:10:49 +0000

    deploy: 02123fb90eeb2b805fe49275ec87ca2c9f12b265

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/undefined.html","posts/reading-summary-2026.html","posts/shakespeare-sonnet-18.html","posts/undefined.html"];function toRandomPost(){
+var posts=["posts/undefined.html","posts/reading-summary-2026.html","posts/shakespeare-sonnet-18.html","posts/undefined.html","posts/zhongkao-physics-video.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
